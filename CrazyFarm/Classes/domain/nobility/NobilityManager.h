60bc3323a1a5cfdaf7345e0c5d5fff3d9ef138ef
moloxiao <molo.xiao@gmail.com>
2016-01-12 16:06:19 +0800

    CrazyFarm
    添加贵族获取接口

--- CrazyFarm/Classes/domain/nobility/NobilityManager.h
+++ CrazyFarm/Classes/domain/nobility/NobilityManager.h
@@ -1,18 +1,33 @@
-#ifndef _PAY_H_
-#define _PAY_H_
+#ifndef _NOBILITYMANAGER_H_
+#define _NOBILITYMANAGER_H_
 #include "cocos2d.h"
 
 using namespace cocos2d;
+
+struct NobilityRewadItem  {
+    int item_id;
+    int num;
+};
 
 class NobilityManager {
 
 public:
     static NobilityManager* getInstance();
     
+    
+    bool isNobility();// 是否为贵族
+    std::vector<NobilityRewadItem> getNobilityRewadItems(); // TODO : 获取单日奖励列表
+    bool addNewDay(std::string day);                        // 添加一个新的签到日期
+    bool isGetRewardToday();                                // 今日是否领取过礼物
+    
 private:
     NobilityManager();
     void init();
     static NobilityManager* _instance;
+    std::vector<NobilityRewadItem> nobilityRewadItems;
+    
+    std::string startDay;
+    int steps;
 };
 
 #endif
