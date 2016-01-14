42854668e517a4a0969e8c1903804491a59c617c
moloxiao <molo.xiao@gmail.com>
2016-01-14 11:04:12 +0800

    CrazyFarm
    奖金池实现金币变更接口

--- CrazyFarm/Classes/domain/bonuspool/BonusPoolManager.h
+++ CrazyFarm/Classes/domain/bonuspool/BonusPoolManager.h
@@ -14,14 +14,17 @@ class BonusPoolManager {
 public:
     static BonusPoolManager* getInstance();
     
-    // TODO : 获取奖金鱼配置信息
-    // TODO : 根据金币数获取奖励区间
+    int getCoins();                 // TODO : 获取奖金池的数目
+    bool addCoins(int coins);       // TODO : 增加奖金池的数目
+    bool cleanCoins();              // TODO : 清空奖金池的数目
+    
+    // TODO : 获取奖励区间和奖励结果
     
 private:
     BonusPoolManager();
     void init();
     static BonusPoolManager* _instance;
     
-    
+    const char* KEY_BONUSPOOL_COINS = "KEY_BONUSPOOL_COINS";
 };
 
