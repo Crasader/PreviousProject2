70555de0a44108f45cb8ed85c4830f3efbaa95ba
chenkj <122358085@qq.com>
2016-01-14 11:16:08 +0800

    捕鱼

--- CrazyFarm/Classes/fish/FishArrangeTwo.cpp
+++ CrazyFarm/Classes/fish/FishArrangeTwo.cpp
@@ -7,6 +7,7 @@ void FishArrangeTwo::initFish(int fishType){
 	fishGold = fishdata.baseReward;
 	this->grabProbability = fishdata.probability;
 	this->fishType = fishType;
+	this->speed = fishdata.move_speed;
 	this->experience = getFishExperienceByType(fishType);
 	initFishAnim(fishdata.uiId);
 }
