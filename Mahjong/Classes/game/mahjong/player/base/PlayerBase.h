d7546ff56ad005fb09359bdc95b5b0483cf7c6c5
qz515880 <1195933889@qq.com>
2016-01-15 18:11:27 +0800

    麻将
    添加打牌倒计时效果

--- Mahjong/Classes/game/mahjong/player/base/PlayerBase.h
+++ Mahjong/Classes/game/mahjong/player/base/PlayerBase.h
@@ -22,5 +22,10 @@ private:
 	Sprite* headimage;
 	Sprite* diamond;
 	LabelAtlas* diamondNum;
+
+	Sprite*          mStencil;       // 蒙板精灵,绿色半透明
+	ProgressTimer*   mProgressTimer; // 时间进度条精灵(360度旋转)
+	float            mCDTime;         // 时间
+	void  timeAnim();
 };
 #endif
