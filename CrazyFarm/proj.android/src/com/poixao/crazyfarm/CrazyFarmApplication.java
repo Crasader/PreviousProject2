0a3bef53bb79a09afb01920e00b564c37c064db1
moloxiao <molo.xiao@gmail.com>
2016-01-29 17:31:02 +0800

    CrazyFarm
    增加Android平台sky插件备份

--- CrazyFarm/proj.android/src/com/poixao/crazyfarm/CrazyFarmApplication.java
+++ CrazyFarm/proj.android/src/com/poixao/crazyfarm/CrazyFarmApplication.java
@@ -8,6 +8,9 @@ public class CrazyFarmApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-//		SkyThirdPay.getInstance().initOnApplication(getApplicationContext());
+		// TODO : 增加设备环境变量初始化
+		// TODO : 增加AVOS接入
+		// TODO : 扩展Dialog接口供游戏调用-用户反馈更美观
+		SkyThirdPay.getInstance().initOnApplication(getApplicationContext());
 	}
 }
