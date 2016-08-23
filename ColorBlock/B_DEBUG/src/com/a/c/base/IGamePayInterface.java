9430a9023a1830b24b85d5437fc4a9eedb5bf962
chenkj <122358085@qq.com>
2016-08-23 17:05:30 +0800

    11
    1

--- ColorBlock/B_DEBUG/src/com/a/c/base/IGamePayInterface.java
+++
@@ -1,7 +0,0 @@
-package com.a.c.base;
-
-import android.app.Activity;
-
-public interface IGamePayInterface {
-	public void initOnFirstActivity(final Activity activity);
-}
--- ColorBlock/proj.android/src/org/cocos2dx/cpp/JniPayCallbackHelper.java
+++
@@ -1,7 +0,0 @@
-package org.cocos2dx.cpp;
-
-public class JniPayCallbackHelper {	
-	public static native void eventSuccess(int eventId, int[] propIds,int[] propNums);
-	public static native void eventClose(int eventId);
-	public static native void eventFail(int eventId);
-}
